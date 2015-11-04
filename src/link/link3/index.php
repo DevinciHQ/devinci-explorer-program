be028979beca9c89152d8e2712bdfe8da4c2dcf7
zbenaissa <zackbenaissa@gmail.com>
2015-11-04 14:12:01 -0500

    Added link php files and link feature file

---
+++ src/link/link3/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<html>";
+echo "<title>link3</title>";
+echo "<p>This is link2</p>";
+echo "</html>";
+?>
+
---
+++ src/link/link4/index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<html>";
+echo "<title>link4</title>";
+echo "<p>This is link4</p>";
+echo "</html>";
+?>
+
