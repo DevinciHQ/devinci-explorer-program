b2b1f1cccfac151008c68a3ca7b8e8905a01cd9b
zbenaissa <zackbenaissa@gmail.com>
2015-11-04 13:04:16 -0500

    Added html.feature and index.php file

---
+++ src/html/index.php
@@ -0,0 +1,11 @@
+<?php
+echo "<html>";
+echo "<title>html test</title>";
+echo "<p>This is a test html page</p>";
+echo "<div>";
+for ($i = 0; $i < 3; $i++){
+	echo "<p>".$i."</p>";
+}
+echo "</div>";
+echo "</html>";
+?>
