15cf95fafc47eed61ab9a09ceb702d49bd5f416e
zbenaissa <zackbenaissa@gmail.com>
2015-11-04 15:21:48 -0500

    Added urlinput php file and urlinput feature file

--- src/urlinput/index.php
+++ src/urlinput/index.php
@@ -1,3 +1,8 @@
 <?php
-echo 'Wow, you are '. htmlspecialchars($_GET["age"]).' years old!';
+if ($_GET["age"] != null){
+  echo 'Wow, you are '. htmlspecialchars($_GET["age"]).' years old';
+}
+if ($_GET["sport"] != null){
+  echo ' and you still play '.htmlspecialchars($_GET["sport"]).'!';
+}
 ?>
