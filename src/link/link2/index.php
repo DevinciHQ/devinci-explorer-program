cc05c3e46aa913fcc9b0e71bcd9f6b36f1948c07
Amit <amit.dinda.prdxn@gmail.com>
2016-09-06 09:59:51 +0530

    Assignments

--- src/link/link2/index.php
+++ src/link/link2/index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<html>";
 echo "<title>link2</title>";
-echo "<p>This is link2</p>";
+echo "<p>This is one</p>";
 echo "</html>";
 ?>
---
+++ test/features/time.feature
@@ -0,0 +1,4 @@
+Feature: Time 
+Scenario:
+Given I am on "/time"
+Then I should see "Thu September 2016"
